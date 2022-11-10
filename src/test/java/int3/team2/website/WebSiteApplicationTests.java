7b1beb66e6799039c27f5060fe782d48e02621dc
tomellm <thomas.ellmenreich@gmail.com>
2022-11-10 15:37:05 +0100

    spring init

---
+++ src/test/java/int3/team2/website/WebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package int3.team2.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

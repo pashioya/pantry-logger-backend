acbaff984a339e836352c97a9d8435d1c03a0c85
pashioya <paul.ashioya@student.kdg.be>
2022-12-05 16:30:47 +0100

    Created Profile Page and related controller

---
+++ src/main/java/int3/team2/website/pantry_loogr/presentation/UserController.java
@@ -0,0 +1,49 @@
+package int3.team2.website.pantry_loogr.presentation;
+
+import int3.team2.website.pantry_loogr.domain.EndUser;
+import int3.team2.website.pantry_loogr.presentation.helper.DataItem;
+import int3.team2.website.pantry_loogr.presentation.helper.HtmlItems;
+import int3.team2.website.pantry_loogr.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+@Controller
+@RequestMapping("/profile")
+public class UserController {
+
+    private Logger logger;
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.logger = Logger.getLogger(this.getClass().getName());
+        this.userService = userService;
+    }
+
+
+    @GetMapping
+    public String profile(HttpSession httpSession, Model model) {
+        EndUser user = userService.authenticate((String) httpSession.getAttribute("username"), (String) httpSession.getAttribute("password"));
+        if(user == null) {
+            return "redirect:/login";
+        }
+        model.addAttribute("title", "Profile");
+        model.addAttribute("headerList", new ArrayList<>(Arrays.asList(
+                new DataItem(HtmlItems.BACK_BUTTON,"/recipes/recommend"),
+                new DataItem(HtmlItems.HEADER_TITLE, "Profile"),
+                new DataItem(HtmlItems.LOGO)
+        )));
+        model.addAttribute("user", user);
+        return "profile";
+    }
+    
+
+
+
+}
